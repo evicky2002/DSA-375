d13d1e812b338d4af03df5ce4c5d3b279dd83ceb
Vignesh E <evicky2002@gmail.com>
2022-07-14 21:40:04 +0530

    added commments

--- Reverse the Array/ReverseTheArray.java
+++ Reverse the Array/ReverseTheArray.java
@@ -2,6 +2,13 @@ import java.util.Arrays;
 
 public class ReverseTheArray {
     public static int[] reverseTheArray(int[] nums) {
+        // Store the moving forward index at position
+        // Loop through the length of the array
+        // Store the last value of array in temp always
+        // Move the rest of the elements one position to the right
+        // After moving, we would have the moving forward index free to be occupied ny
+        // the temp
+        // Now increment position and repeat until the array is reversed
         int position = 0;
         for (int i = nums.length - 1; i >= 0; i--) {
             int temp = nums[nums.length - 1];
